a8df4e9b3b04a621ef146167e283adb012d2a215
Karajan421 <63369617+Karajan421@users.noreply.github.com>
2022-01-10 17:56:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,64 @@
+import pandas as pd
 import flask
 import difflib
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from wordcloud import WordCloud, STOPWORDS
+
+result = pd.read_csv("merged_impact_appstore.csv")
+result = result.drop_duplicates(subset=['title'])
+result = result[50000:]
+#result1 = result.loc[result['genre'] == TARGET_GENRE]
+result1 = result.reset_index(drop=True)
+result1["description"] = result1["description"].fillna("")
+result1["processed_desc1"] = result1["processed_desc1"].fillna("")
 
 app = flask.Flask(__name__, template_folder='templates')
 
-df2 = pd.read_csv('./model/tmdb.csv')
+#full_text = list(train['description'].values) #+ list(test['Phrase'].values)
+#vectorizer = TfidfVectorizer (max_features=2500, ngram_range=(1, 3), min_df=7, max_df=0.8, stop_words=stopwords.words('english'))
+#vectorizer.fit_transform(full_text).toarray()
+#vectorizer.fit_transform(full_text)
+#train_vectorized = vectorizer.transform(train['description'])
+#test_vectorized = vectorizer.transform(test['description'])
 
-count = CountVectorizer(stop_words='english')
-count_matrix = count.fit_transform(df2['soup'])
+stopwords_list = STOPWORDS.union(set(['Games', 'the', 'to', 'will', 'make', 'play', 'level', 'get', 'use', 
+                                      "samsung", "Samsung", "galaxy", "Galaxy", "xiaomi", "pixel", "google", 
+                                      'game', "games", "list", "and", "for", "price", "quot", "it", 'we', 'don', 
+                                      'you', 'ce', 'hasn', 'sa', 'do', 'som', 'can']))
+        # Some words which might indicate a certain sentiment are kept via a whitelist
+vectorizer= TfidfVectorizer(max_features = 2500, ngram_range=(1,2), min_df=7, max_df=0.8, 
+                            stop_words=stopwords_list)
 
-cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+#Construct the required TF-IDF matrix by applying the fit_transform method on the overview feature
+overview_matrix = vectorizer.fit_transform(result1["processed_desc1"])
+similarity_matrix = linear_kernel(overview_matrix,overview_matrix)
+#games index mapping
+mapping = pd.Series(result1.index,index = result1["title"])
+# Compute the cosine similarity matrix
+cosine_sim = linear_kernel(overview_matrix,overview_matrix)
 
-df2 = df2.reset_index()
-indices = pd.Series(df2.index, index=df2['title'])
-all_titles = [df2['title'][i] for i in range(len(df2['title']))]
+def get_recommendations(app, cosine_sim=cosine_sim):
+    
+    idx = mapping[app]
+    #get similarity values with other games
+    #similarity_score is the list of index and similarity matrix
+    similarity_score = list(enumerate(cosine_sim[idx]))
+    #sort in descending order the similarity score of game inputted with all the other games
+    similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    # Get the scores of the 15 most similar games. Ignore the first game.
+    similarity_score = similarity_score[1:25]
+    #return game names using the mapping series
+    result_indices = [i[0] for i in similarity_score]
+    
+    result_score = [i[1] for i in similarity_score]
+    
+    result2 = result1.iloc[result_indices]
+    result2["score"] = result_score
+    #return (result1.iloc[result_indices])
+    return result2
 
-def get_recommendations(title):
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:11]
-    movie_indices = [i[0] for i in sim_scores]
-    tit = df2['title'].iloc[movie_indices]
-    dat = df2['release_date'].iloc[movie_indices]
-    return_df = pd.DataFrame(columns=['Title','Year'])
-    return_df['Title'] = tit
-    return_df['Year'] = dat
-    return return_df
 
 # Set up the main route
 @app.route('/', methods=['GET', 'POST'])
