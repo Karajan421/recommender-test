12e1a7c433743758a7a8230e904cbee06f30b489
Karajan421 <63369617+Karajan421@users.noreply.github.com>
2022-01-11 09:48:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 app = flask.Flask(__name__, template_folder='templates')
 
 df2 = pd.read_csv('./model/small_model.csv')
-df2 = df2[95000:]
 df2 = df2.reset_index(drop=True)
 df2["description"] = df2["description"].fillna("")
 df2["processed_desc1"] = df2["processed_desc1"].fillna("")
