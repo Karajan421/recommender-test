cf0ee352ee196624cb605d727d6b838293982f81
Karajan421 <63369617+Karajan421@users.noreply.github.com>
2022-01-11 14:18:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,23 +6,23 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 app = flask.Flask(__name__, template_folder='templates')
 
-df2 = pd.read_csv('./model/small_model3.csv')
+df2 = pd.read_csv('./model/miria_alpha_appstore.csv')
 #df2 = df2[95000:]
 df2 = df2.reset_index(drop=True)
-df2["description"] = df2["description"].fillna("")
-df2["processed_desc1"] = df2["processed_desc1"].fillna("")
+#df2["description"] = df2["description"].fillna("")
+#df2["processed_desc1"] = df2["processed_desc1"].fillna("")
 
 count = CountVectorizer(stop_words='english')
 
-count_matrix = count.fit_transform(df2['processed_desc1'])
+count_matrix = count.fit_transform(df2['App_Name'])
 
 
 
 cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
 
 #df2 = df2.reset_index()
-indices = pd.Series(df2.index, index=df2['title'])
-all_titles = [df2['title'][i] for i in range(len(df2['title']))]
+indices = pd.Series(df2.index, index=df2['App_Name'])
+all_titles = [df2['App_Name'][i] for i in range(len(df2['App_Name']))]
 
 def get_recommendations(title) :
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
@@ -31,11 +31,11 @@ def get_recommendations(title) :
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
-    tit = df2['title'].iloc[movie_indices]
-    dat = df2['genre'].iloc[movie_indices]
-    return_df = pd.DataFrame(columns=['Title','Year'])
+    tit = df2['App_Name'].iloc[movie_indices]
+    dat = df2['AppStore_Url'].iloc[movie_indices]
+    return_df = pd.DataFrame(columns=['Title','Link'])
     return_df['Title'] = tit
-    return_df['Year'] = dat
+    return_df['Link'] = dat
     return return_df
 
 #Set up the main route
